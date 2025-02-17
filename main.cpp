de4eef1e5ea5f6abc88fdfe0e62c3ff2e3fb9797
alannchang <alannnchang@gmail.com>
2025-02-16 21:47:44 -0800

    input textbox now responds to Return key

--- main.cpp
+++ main.cpp
@@ -4,15 +4,29 @@
 
 using namespace ftxui;
 
+const std::string MAIMAIL_VERSION = "v1.0.0";
+
 int main() {
   auto screen = ScreenInteractive::Fullscreen();
 
   int selected = 0;
   std::vector<std::string> options = {"Option 1", "Option 2", "Option 3"};
 
+  std::string command_text = "";
+
   auto menu = Menu(&options, &selected);
   std::string input_string;
-  auto user_input = Input(&input_string, "Type commands here...") | border;
+  Component user_input = Input(&input_string, "Type commands here...") |
+      border |
+      CatchEvent([&](Event event) {
+          if (event == Event::Return && !input_string.empty()) {
+              command_text += " " + input_string;
+              input_string.clear();
+              return true;
+          }
+          return false;
+      });
+
 
   auto container = Container::Vertical({
       menu, 
@@ -21,9 +35,10 @@ int main() {
 
   auto renderer = Renderer(container, [&] {
     return vbox({
-        text("Welcome to MaiMail") | center,
+        text("MaiMail " + MAIMAIL_VERSION) | center,
         separator(),
         menu->Render() | flex,
+        text(command_text),
         separator(),
         user_input->Render()
     });
